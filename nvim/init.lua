61b616b969ac351b986d0d55da1eb9d8e492189b
Maciej Janicki <mjanicki@internships.antmicro.com>
2025-05-14 15:14:59 +0200

    feat: good configuration - coming back to lsps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,43 +1,31 @@
-vim.opt.tabstop = 4       -- Number of visual spaces per tab
-vim.opt.softtabstop = 4   -- Number of spaces in tab when editing
-vim.opt.shiftwidth = 4    -- Number of spaces in tab
+-- Basic settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.number = true     -- show line numbers
-vim.opt.splitbelow = true -- horizontal split opens at a bottom
-vim.opt.splitright = true -- vertical split opens at the right
+vim.opt.number = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.history = 1000
+vim.opt.shortmess:append("c")
+vim.opt.cursorline = true
+vim.opt.mouse:append("a")
 
-vim.opt.incsearch = true  -- search as characters are entered
-vim.opt.ignorecase = true
-vim.opt.smartcase = true  -- do not ignore uppercase
-
--- show trailing spaces
+-- Trailing whitespace and tabs
 vim.opt.list = true
-vim.opt.listchars:append({ trail = "·", tab = "» " })
+vim.opt.listchars = { trail = "·", tab = "» " }
 
-local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-if is_windows then
-    vim.o.makeprg = '.\build.bat'
-else
-    vim.opt.path:append({"/usr/include", "/usr/local/include", "/usr/include/c++"})
-    vim.o.makeprg = './build.sh'
-end
+-- Search settings
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 
+-- Bell
+vim.opt.belloff = "all"
 
-
-vim.api.nvim_set_keymap("n", "<F5>", ":make<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<F4>", "<C-w>v:copen<CR><C-w>k:q<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<F8>", ":!ctags -R --exclude=.git --exclude=bin --exclude=obj .<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gd', ':grep! -r "<C-R><C-W>" %<CR>', { noremap = true, silent = true })
--- --include \\*.%:e
-vim.api.nvim_set_keymap('n', 'gD', ':grep! -r "<C-R><C-W>"  .<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<TAB>', ':bn<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bp<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F3>', ':Ex<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
-
---- vim.opt.spell = true
-vim.opt.guicursor = "n-v-c:blinkon1-blinkoff1"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -53,29 +41,23 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    "RRethy/base16-nvim",
-    "nvim-treesitter/nvim-treesitter",
+    "NLKNguyen/papercolor-theme",
+    "neovim/nvim-lspconfig",
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
-    "ray-x/cmp-treesitter",
+    'hrsh7th/cmp-nvim-lsp',
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/nvim-cmp",
-})
 
-require("nvim-treesitter.configs").setup({
-  highlight = {enable = false},
-  })
+})
 
 cmp = require'cmp'
 require("cmp").setup({
-    snippet = {
-        expand = function(args) 
-            vim.snippet.expand(args.body)
-        end,
-    },
     sources = cmp.config.sources(
       {
-        { name = 'treesitter'},
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lsp' },
         { name = 'buffer' },
         { name = 'path' },
     }),
@@ -84,21 +66,18 @@ require("cmp").setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-
 })
+
 
 
 vim.opt.pumheight = 5
 vim.opt.pumwidth = 20
 
--- vim.o.background = "light"
+-- Theme and color
 vim.opt.termguicolors = true
-vim.cmd("colorscheme base16-selenized-white")
-vim.cmd("set nowrap")
-
-vim.opt.tags = "./tags,tags;"
-vim.opt.undofile = true   -- Enable persistent undo
-vim.opt.mouse = 'a'       -- Enable mouse support
-vim.o.tags = './tags;,tags'
+vim.o.background = "light"
+vim.cmd.colorscheme("PaperColor")
+vim.lsp.enable('pyright')
+vim.lsp.enable('svls')
+vim.diagnostic.config({ underline = true, virtual_lines = true, signs = false})
